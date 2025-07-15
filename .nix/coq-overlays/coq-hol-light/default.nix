c975655b32c8f5f8b349e4e3e65faa2be7c88572
Abdelghani ALIDRA <alidrandco@yahoo.fr>
2025-07-15 17:44:01 +0200

    github actions generated with nix

---
+++ .nix/coq-overlays/coq-hol-light/default.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  mkCoqDerivation,
+  rocq-core,
+  coq,
+  stdlib,
+  version ? null,
+}:
+
+(mkCoqDerivation {
+  pname = "coq-hol-light";
+  owner = "deducteam";
+  repo = "coq-hol-light";
+  opam-name = "coq-hol-light";
+  inherit version;
+  defaultVersion = "3.0.0";
+  release = {
+    "3.0.0".sha256 = "sha375ae92b74b652e868cd5b87f3b2e50e58f58bed";
+  #  "master";
+  };
+  releaseRev = v: "v${v}";
+
+  mlPlugin = true;
+  useDune = true;
+
+  propagatedBuildInputs = [ coq stdlib rocq-core.ocamlPackages.ppx_optcomp ];
+
+  meta = with lib; {
+    homepage = "https://github.com/Deducteam/coq-hol-light-real/";
+    description = "automatic translation of a subset of the HOL-Light base library";
+    maintainers = with maintainers; [ fblanqui ];
+  };
+})
